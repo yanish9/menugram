e602f69206595f1a77bdeff56aa462eb0c4ff1bf
yanish9 <yanishguru@gmail.com>
2017-01-28 10:28:20 -0500

    oka

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -5,9 +5,17 @@ $(".search-btn").on("click", showMap);
 
 function showMap() {
 	// body... 
+
+	var map_side= "<div class= 'col-sm-5' id= 'map_side'> <div>"
+	var side_bar = "<div class= 'col-sm-7' id= 'side_bar'> <div>"
+
+
 	var map = $('<div id="googleMap" style="width:100%;height:500px;"></div>');
 
-	$("#map").html(map);
+	$("#map_side").html(map);
+
+$("#map").html("");
+	$("#map").append(map_side,side_bar);
 
 	var mapProp= {
     center:new google.maps.LatLng(51.508742,-0.120850),
